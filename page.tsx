2e29d6ac7c834a69a0b74d4b372e0e03672681d0
fukunaga-faise <fukunaga@faise-inc.com>
2026-07-08 17:47:54 +0900

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,27 @@
+import Nav from '@/components/Nav'
+import Hero from '@/components/Hero'
+import Mission from '@/components/Mission'
+import Services from '@/components/Services'
+import Works from '@/components/Works'
+import Clients from '@/components/Clients'
+import Founder from '@/components/Founder'
+import Company from '@/components/Company'
+import CTA from '@/components/CTA'
+import PageCurtain from '@/components/PageCurtain'
+
+export default function Home() {
+  return (
+    <main className="bg-white text-[#101318]">
+      <PageCurtain />
+      <Nav />
+      <Hero />
+      <Mission />
+      <Services />
+      <Works />
+      <Clients />
+      <Founder />
+      <Company />
+      <CTA />
+    </main>
+  )
+}
